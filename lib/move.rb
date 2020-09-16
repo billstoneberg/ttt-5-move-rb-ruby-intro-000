44ad685a913f6926af9b2e0afef7fd1d091eba1c
William Stoneberg <wrstoneberg@me.com>
2020-09-16 23:19:42 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,6 +1,3 @@
-move = gets.strip
-input_to_index = move.to_i
-board = input_to_index - 1
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -8,8 +5,5 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-move = board
-puts "Welcome to Tic-Tac-Toe"
-puts "What move would like like to make using 1 - 9?"
 
 # code your input_to_index and move method here!
